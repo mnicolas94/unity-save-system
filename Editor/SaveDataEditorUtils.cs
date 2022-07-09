36021a1d5898173411202475800a407d283ce444
mnicolas94 <miguelcok27@gmail.com>
2022-07-09 10:19:25 -0400

    FEAT: add reset data menu item

---
+++ Editor/SaveDataEditorUtils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+
+namespace SaveSystem.Editor
+{
+    public static class SaveDataEditorUtils
+    {
+        [MenuItem("Tools/Facticus/SaveSystem/Reset data")]
+        public static void ResetData()
+        {
+            var persistentObjects = SaveUtils.GetAllPersistentObjects();
+            foreach (var persistent in persistentObjects)
+            {
+                persistent.ResetToDefault();
+            }
+        }
+    }
+}
