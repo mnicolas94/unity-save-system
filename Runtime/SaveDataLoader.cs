c69bbac5e7d5a49661338e5b7a1575854df96857
mnicolas94 <miguelcok27@gmail.com>
2023-03-15 10:15:57 -0400

    refactor: change the way SaveDataLoader works. It now has a list of scriptable objects to load, instead of "automatically" obtain them

--- Runtime/SaveDataLoader.cs
+++ Runtime/SaveDataLoader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -10,6 +11,7 @@ namespace SaveSystem
     {
         [SerializeField] private UnityEvent _onLoadingFinish;
         [SerializeField] private UnityEvent<float> _onLoadingProgress;
+        [SerializeField] private List<ScriptableObject> _data;
 
         private CancellationTokenSource _cts;
 
@@ -27,46 +29,20 @@ namespace SaveSystem
 
         private async void LoadSaveDataAsync(CancellationToken ct)
         {
-#if UNITY_EDITOR
-            await Task.Yield();
-            _onLoadingProgress.Invoke(0.9f);
-#else
             float loadPersistentsProgress = 0.5f;
-            var persistentObjects = SaveUtils.GetAllPersistentObjects();
             _onLoadingProgress.Invoke(loadPersistentsProgress);
-            int count = persistentObjects.Count;
+            int count = _data.Count;
             for (int i = 0; i < count; i++)
             {
-                var persistent = persistentObjects[i];
-                var fileName = persistent.GetPersistentFileName();
-                Debug.Log($"- SaveDataLoader.LoadSaveDataAsync: loading {fileName}");
-                var loadReport = new SaveUtils.LoadReport{Success = false};
-                try
-                {
-                    loadReport = await persistent.Load();
-                }
-                catch (Exception e)
-                {
-                    Debug.LogError($"An exception was thrown during data loading: {fileName}");
-                    Debug.LogError(e.ToString());
-                }
-                
-                if (!loadReport.Success)
-                {
-                    persistent.ResetToDefault();
-                    await persistent.Save();
-                }
-                
-                Debug.Log($"- SaveDataLoader.LoadSaveDataAsync: load report:\n{loadReport.ToString()}");
-
                 if (ct.IsCancellationRequested)
                     break;
                 
-                await Task.Yield();
+                var persistent = _data[i];
+                await persistent.LoadOrCreate();
+                
                 float progress = loadPersistentsProgress + (float)(i + 1) / count * (1 - loadPersistentsProgress);
                 _onLoadingProgress.Invoke(progress);
             }
-#endif
             _onLoadingFinish.Invoke();
         }
     }
