6d29ac1311cdc31eea110bb093ec5799e6985f21
mnicolas94 <miguelcok27@gmail.com>
2024-01-18 10:08:14 -0500

    fix(AssetSaver.cs): await async function in AssetSaver.Save() to propagate exceptions

--- Runtime/AssetSaver.cs
+++ Runtime/AssetSaver.cs
@@ -5,9 +5,9 @@ namespace SaveSystem
     [CreateAssetMenu(fileName = "AssetSaver", menuName = "Facticus/Save system/AssetSaver", order = 0)]
     public class AssetSaver : ScriptableObject
     {
-        public void Save(ScriptableObject obj)
+        public async void Save(ScriptableObject obj)
         {
-            obj.Save();
+            await obj.Save();
         }
     }
 }
