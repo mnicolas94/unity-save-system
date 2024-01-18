64570aaeb315b5bc361c131481a24af980f04e1e
mnicolas94 <miguelcok27@gmail.com>
2024-01-18 18:20:22 -0500

    feat(serializers): implement a new serializer with com.unity.serialization package

---
+++ Runtime/Serializers/UnitySerializer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Collections.LowLevel.Unsafe.NotBurstCompatible;
+using Unity.Serialization.Binary;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace SaveSystem.Serializers
+{
+    [Serializable]
+    public class UnitySerializer : ISerializer
+    {
+        public byte[] Serialize(ScriptableObject obj, AssetGuidsDatabase guidsDatabase)
+        {
+            return Serialize<ScriptableObject>(obj, guidsDatabase);
+        }
+        
+        public unsafe byte[] Serialize<T>(T obj, AssetGuidsDatabase guidsDatabase) where T : Object
+        {
+            using var stream = new UnsafeAppendBuffer(128, 8, Allocator.Temp);
+            var unsafeAppendBuffer = stream;
+            var parameters = GetBinarySerializationParameters(guidsDatabase);
+
+            BinarySerialization.ToBinary(&unsafeAppendBuffer, obj, parameters);
+            var data = unsafeAppendBuffer.ToBytesNBC();
+            
+            return data;
+        }
+
+        public void Deserialize(byte[] data, ScriptableObject obj, AssetGuidsDatabase guidsDatabase)
+        {
+            Deserialize<ScriptableObject>(data, obj, guidsDatabase);
+        }
+        
+        public unsafe void Deserialize<T>(byte[] data, T obj, AssetGuidsDatabase guidsDatabase) where T : Object
+        {
+            using var nativeArray = new NativeArray<byte>(data, Allocator.Temp);
+
+            var reader = new UnsafeAppendBuffer.Reader(nativeArray.GetUnsafePtr(), nativeArray.Length);
+
+            var parameters = GetBinarySerializationParameters(guidsDatabase);
+            var deserialized = BinarySerialization.FromBinary<T>(&reader, parameters);
+            
+            ReflectionUtils.CopyTo(deserialized, obj);
+        }
+        
+        private static BinarySerializationParameters GetBinarySerializationParameters(
+            AssetGuidsDatabase guidsDatabase)
+        {
+            var parameters = new BinarySerializationParameters
+            {
+                UserDefinedAdapters = new List<IBinaryAdapter> { new GuidBinaryAdapter(guidsDatabase) },
+                DisableRootAdapters = true
+            };
+            return parameters;
+        }
+    }
+
+    public class GuidBinaryAdapter : IContravariantBinaryAdapter<Object>
+    {
+        private AssetGuidsDatabase _database;
+
+        public GuidBinaryAdapter(AssetGuidsDatabase database)
+        {
+            _database = database;
+        }
+
+        public void Serialize(IBinarySerializationContext context, Object value)
+        {
+            var guid = "";
+            if (_database != null)
+            {
+                guid = _database.TryGetGuid(value, out bool exists);
+            }
+            
+            context.SerializeValue(guid);
+        }
+
+        public object Deserialize(IBinaryDeserializationContext context)
+        {
+            if (_database != null)
+            {
+                var guid = context.DeserializeValue<string>();
+                var value = _database.TryGetObject(guid, out var exists);
+                
+                return value;
+            }
+            
+            return null;
+        }
+    }
+}
---
+++ Tests/Editor/SerializersTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SaveSystem.Serializers;
+using UnityEngine;
+
+namespace SaveSystem.Tests.Editor
+{
+    public class SerializersTests
+    {
+        private const string UnitySerializerKey = "UnitySerializer";
+        private const string OdinSerializerKey = "OdinSerializer";
+
+        private readonly Dictionary<string, ISerializer> _serializers = new Dictionary<string, ISerializer>
+        {
+            { UnitySerializerKey, new UnitySerializer() },
+            { OdinSerializerKey, new OdinPersistentSerializer() },
+        };
+        
+        [TestCase(UnitySerializerKey)]
+        [TestCase(OdinSerializerKey)]
+        public void WhenSerializeAnObject_ItsDeserializationHasTheProperTheValues_Test(string serializerKey)
+        {
+            // arrange
+            var serializer = _serializers[serializerKey];
+            var expected = ScriptableObject.CreateInstance<PersistentObject>();
+            expected.I = 42;
+            expected.S = "Hello serializer";
+            expected.Lb = new List<bool> { true, false, true };
+                
+            // act
+            var bytes = serializer.Serialize(expected, null);
+            var deserialized = ScriptableObject.CreateInstance<PersistentObject>();
+            serializer.Deserialize(bytes, deserialized, null);
+
+            // assert
+            Assert.AreEqual(expected.I, deserialized.I);
+            Assert.AreEqual(expected.S, deserialized.S);
+            Assert.AreEqual(expected.Lb, deserialized.Lb);
+        }
+    }
+
+    public class PersistentObject : ScriptableObject
+    {
+        public string S;
+        public int I;
+        public List<bool> Lb;
+        public PersistentObject _reference;
+    }
+}
