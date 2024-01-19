059ff3cd4831a5861168879b226c2eccdf68550f
mnicolas94 <miguelcok27@gmail.com>
2024-01-19 16:31:38 -0500

    tests: implement runtime tests to test on builds

---
+++ Samples/TestRuntimeSaveLoad.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using SaveSystem;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Samples
+{
+    public class TestRuntimeSaveLoad : MonoBehaviour
+    {
+        [SerializeField] private TestRuntimeSaveLoadData _data;
+
+        [SerializeField] private Button _button;
+        
+        [SerializeField] private float _headerScale;
+        [SerializeField] private TextMeshProUGUI _beforeSaveText;
+        [SerializeField] private TextMeshProUGUI _afterSaveChangeText;
+        [SerializeField] private TextMeshProUGUI _afterLoadText;
+        
+        private async void Start()
+        {
+            await SaveAndLoad();
+            _button.onClick.AddListener(OnClick);
+        }
+
+        private async void OnClick()
+        {
+            _button.interactable = false;
+            
+            await SaveAndLoad();
+            
+            _button.interactable = true;
+        }
+
+        private async Task SaveAndLoad()
+        {
+            _data.Randomize();
+            SetDataToText(_beforeSaveText);
+            await _data.Save();
+
+            _data.Randomize();
+            SetDataToText(_afterSaveChangeText);
+
+            await _data.Load();
+            SetDataToText(_afterLoadText);
+        }
+
+        private void SetDataToText(TextMeshProUGUI text)
+        {
+            var json = JsonUtility.ToJson(_data, true);
+            text.text = $"<size={_headerScale * 100}%>{text.name}:\n<size=100%>{json}";
+        }
+    }
+}
---
+++ Samples/TestRuntimeSaveLoadData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Samples
+{
+    [CreateAssetMenu(fileName = "TestRuntimeSaveLoadData", menuName = "Samples/TestRuntimeSaveLoadData", order = 0)]
+    public class TestRuntimeSaveLoadData : ScriptableObject
+    {
+        public int IntField;
+        public string StringField;
+        public float FloatField;
+        public bool BoolField;
+        public Vector3 Position;
+    
+        public void Randomize()
+        {
+            IntField = (int)(Random.value * 100000);
+            StringField = Random.value.ToString();
+            FloatField = Random.value;
+            BoolField = Random.value > 0.5f;
+            Position = Random.insideUnitSphere;
+        }
+    }
+}
