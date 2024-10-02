dbedaba661ed3f2ef3a2a3e911f978bf8c6113bf
mnicolas94 <miguelcok27@gmail.com>
2024-10-02 09:05:44 -0400

    feat: add SaveGroup.cs

---
+++ Runtime/Utilities/SaveGroup.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SaveSystem.Attributes;
+using UnityEngine;
+
+namespace SaveSystem.Utilities
+{
+    [CreateAssetMenu(fileName = "SaveGroup", menuName = "Facticus/SaveSystem/SaveGroup", order = 0)]
+    public class SaveGroup : ScriptableObject
+    {
+        [SerializeField, DoNotPersist] private List<ScriptableObject> _objects;
+
+        public async Task Save()
+        {
+            foreach (var obj in _objects)
+            {
+                await obj.Save();
+            }
+        }
+        
+        public async Task<SaveUtils.LoadReport> Load()
+        {
+            SaveUtils.LoadReport report = default;
+            foreach (var obj in _objects)
+            {
+                report = await obj.Load();
+            }
+
+            return report;
+        }
+
+        public async Task<SaveUtils.LoadReport> LoadOrCreate()
+        {
+            SaveUtils.LoadReport report = default;
+            foreach (var obj in _objects)
+            {
+                report = await obj.LoadOrCreate();
+            }
+
+            return report;
+        }
+        
+        public void ResetToDefault()
+        {
+            foreach (var obj in _objects)
+            {
+                obj.ResetToDefault();
+            }
+        }
+        
+        private void OnValidate()
+        {
+            if (_objects.Contains(this))
+            {
+                _objects.Remove(this);
+                Debug.LogWarning("Can't add itself to SaveGroup", this);
+            }
+        }
+    }
+}
