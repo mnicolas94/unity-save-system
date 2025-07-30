dd3b41c1002859df8fe15ec878ffe72827d22217
mnicolas94 <miguelcok27@gmail.com>
2025-07-30 14:34:39 -0400

    feat: implement IdReference.cs

---
+++ Editor/PropertyDrawers/SerializedIdPropertyDrawer.cs
@@ -0,0 +1,36 @@
+﻿using SaveSystem.Utilities;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace SaveSystem.Editor.PropertyDrawers
+{
+    [CustomPropertyDrawer(typeof(IdReference<>), true)]
+    public class SerializedIdPropertyDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            var idProperty = property.FindPropertyRelative(IdReference.IdFieldName);
+            var boxedValue = property.boxedValue;
+
+            var field = new ObjectField();
+            field.objectType = boxedValue.GetType().GenericTypeArguments[0];
+            field.label = property.displayName;
+            field.AddToClassList("unity-base-field__aligned");  // make widths aligned with other fields in inspector
+            
+            var currentObject = (boxedValue as IdReference).Object;
+            field.SetValueWithoutNotify(currentObject);
+            field.RegisterValueChangedCallback((evt) =>
+            {
+                var newObject = evt.newValue;
+                if (SaveSystemSettings.Instance.GuidsResolver.TryGetGuid(newObject, out var newId))
+                {
+                    idProperty.stringValue = newId;
+                    idProperty.serializedObject.ApplyModifiedProperties();
+                }
+            });
+            
+            return field;
+        }
+    }
+}
---
+++ Runtime/Utilities/IdReference.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace SaveSystem.Utilities
+{
+    public abstract class IdReference
+    {
+        public const string IdFieldName = nameof(_id);
+      
+        [SerializeField] private string _id;
+        
+        private Object _cachedObject;
+        private string _cachedId;
+        public Object Object
+        {
+            get
+            {
+                if (_cachedId != _id)
+                {
+                    var resolver = SaveSystemSettings.Instance.GuidsResolver;
+                    if (resolver.TryGetObject(_id, out _cachedObject))
+                    {
+                        _cachedId = _id;
+                    }
+                }
+
+                return _cachedObject;
+            }
+            set
+            {
+                if (_cachedId == _id && _cachedObject == value) return;
+                
+                var resolver = SaveSystemSettings.Instance.GuidsResolver;
+                if (resolver.TryGetGuid(value, out var id))
+                {
+                    _id = id;
+                    _cachedId = id;
+                    _cachedObject = value;
+                }
+            }
+        }
+    }
+    
+    /// <summary>
+    /// An object that can accept a value of type T in the inspector, but will be serialized as a unique Id instead
+    /// of the actual object. The object can be accessed with the <see cref="get_Object"/> property.
+    /// It uses the save system's reference resolver (see <see cref="IGuidResolver"/>) to retrieve the objects and ids.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [Serializable]
+    public class IdReference<T> : IdReference where T : Object
+    {
+        public new T Object
+        {
+            get => base.Object as T;
+            set => base.Object = value;
+        }
+        
+        public static implicit operator T(IdReference<T> sId)
+        {
+            return sId.Object;
+        }
+    }
+}
