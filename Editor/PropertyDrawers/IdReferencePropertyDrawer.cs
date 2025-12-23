05fcc493ae0cbca450df5a46a8eb0af4bbfc3b48
Miguel Nicolás-Díaz <35781652+mnicolas94@users.noreply.github.com>
2025-12-23 16:58:19 -0500

    fix: add IMGUI support to IdReferencePropertyDrawer.cs

--- Editor/PropertyDrawers/IdReferencePropertyDrawer.cs
+++ Editor/PropertyDrawers/IdReferencePropertyDrawer.cs
@@ -38,5 +38,41 @@ namespace SaveSystem.Editor.PropertyDrawers
             
             return field;
         }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var idProperty = property.FindPropertyRelative(IdReference.IdFieldName);
+            var idReference = (IdReference) property.boxedValue;
+
+            EditorGUI.BeginChangeCheck();
+            var newObject = EditorGUI.ObjectField(
+                position,
+                property.displayName,
+                idReference.Object,
+                idReference.GetReferencedType(),
+                false);
+            if (EditorGUI.EndChangeCheck())
+            {
+                if (SaveSystemSettings.Instance.GuidsResolver.TryGetGuid(newObject, out var newId))
+                {
+                    idProperty.stringValue = newId;
+                    idProperty.serializedObject.ApplyModifiedProperties();
+                }
+                else if (newObject == null)
+                {
+                    idProperty.stringValue = "";
+                    idProperty.serializedObject.ApplyModifiedProperties();
+                }
+                else
+                {
+                    Debug.LogWarning($"No guid found for {newObject}. You might solve it by populating the guids database");
+                }
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
     }
 }
